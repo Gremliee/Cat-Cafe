ba2869ad35b51a6574408788cfbf68bb43abc5bf
Gremliee <129997686+Gremliee@users.noreply.github.com>
2023-04-15 16:59:59 -0700

    orders

---
+++ CAT_CAFE/Assets/Scripts/Orders.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orders : MonoBehaviour
+{
+    [SerializeField] GameObject Dessert;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
